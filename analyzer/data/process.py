8572bc03a9b6f0f2058d8487a1df91ad06a93f9b
gxxxh <guohao171@mails.ucas.ac.cn>
2023-04-21 15:39:33 +0800

    update pyprof

--- analyzer/data/process.py
+++ analyzer/data/process.py
@@ -9,8 +9,9 @@ def aggregate(dir):
     # Aggregate all the csv files in the directory
     # and return a dataframe
     for root, _, files in os.walk(dir):
+        i = 0
         for file in files:
-            if file.endswith(".csv") and file < "conv2d400.csv":
+            if file.endswith(".csv") :
                 file_path = os.path.join(root, file)
                 df = pd.read_csv(file_path)
                 df["file"] = file_path
@@ -18,6 +19,10 @@ def aggregate(dir):
                     df_all = df
                 else:
                     df_all = pd.concat([df_all, df], ignore_index=True)
+            i+=1
+            if i%50==0:
+                print("forward kernel "+str(len(df_all[df_all["dir"]=="fprop"]["kernel"].unique())))
+                print("backward kernel " + str(len(df_all[df_all["dir"] == "bprop"]["kernel"].unique())))
     return df_all
 
 
